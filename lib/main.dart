3734aca8c28a09a701fad0e55f1137c268001994
Voldy13 <morekunal1335@gmail.com>
2023-02-16 14:56:44 +0530

    commit
    mymap changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
 
 void main() => runApp(MyApp());
 
@@ -138,49 +150,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   }
 }
 
-//hello
-class MyMap extends StatefulWidget {
-  @override
-  State<MyMap> createState() => _MyMapState();
-}
-
-class _MyMapState extends State<MyMap> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Container(
-        child: Column(
-          children: [
-            Flexible(
-                child: FlutterMap(
-              options: MapOptions(center: LatLng(19.0760, 72.8777), zoom: 8),
-              children: [
-                TileLayer(
-                  urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-                ),
-                MarkerLayer(
-                  markers: [
-                    Marker(
-                      point: LatLng(19.044876, 73.079747),
-                      width: 80,
-                      height: 80,
-                      builder: (context) => Icon(Icons.pin_drop),
-                    ),
-                    Marker(
-                      point: LatLng(19.046957, 72.911960),
-                      width: 80,
-                      height: 80,
-                      builder: (context) => Icon(Icons.pin_drop),
-                    ),
-                  ],
-                ),
-              ],
-            ))
-          ],
-        ),
-      )),
-    );
-  }
-}
+//add mymap to moved to mymap.dart
+//reffer following link
+//https://www.dbestech.com/tutorials/flutter-google-sign-in-firebase-auth-login
