78ae1af1fda5d0cf34ac4acde7272b8a30ed94ec
prathamskk <prathamskk@gmail.com>
2023-03-31 00:49:03 +0530

    added 3d model truck

--- lib/screens/new_home_screen.dart
+++ lib/screens/new_home_screen.dart
@@ -4,6 +4,7 @@ import 'package:fragileitemtrackingapp/models/user_model.dart';
 import 'package:fragileitemtrackingapp/providers/user_provider.dart';
 import 'package:fragileitemtrackingapp/services/auth_methods.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_cube/flutter_cube.dart';
 
 class NewHomeScreen extends StatefulWidget {
   const NewHomeScreen({Key? key}) : super(key: key);
@@ -15,33 +16,39 @@ class NewHomeScreen extends StatefulWidget {
 var _textStyleblack = const TextStyle(fontSize: 12.0, color: Colors.black);
 
 class _NewHomeScreenState extends State<NewHomeScreen> {
-    String angleX = "";
-    String angleY = "";
-    String angleZ = "";
-
+  String angleX = "";
+  String angleY = "";
+  String angleZ = "";
+  Object car = Object(fileName: 'asset/truck/Tusk_Truck.obj', lighting: true);
   @override
   void initState() {
     super.initState();
-DatabaseReference refX = FirebaseDatabase.instance.ref("gyro/anglex");
-DatabaseReference refY = FirebaseDatabase.instance.ref("gyro/angley");
-DatabaseReference refZ = FirebaseDatabase.instance.ref("gyro/anglez");
-    refX.onValue.listen((DatabaseEvent event){
+    DatabaseReference refX = FirebaseDatabase.instance.ref("gyro/anglex");
+    DatabaseReference refY = FirebaseDatabase.instance.ref("gyro/angley");
+    DatabaseReference refZ = FirebaseDatabase.instance.ref("gyro/anglez");
+    refX.onValue.listen((DatabaseEvent event) {
       setState(() {
-      angleX = event.snapshot.value.toString();
+        angleX = event.snapshot.value.toString();
+        car.rotation.setValues(double.tryParse(angleX) ?? 0,
+            double.tryParse(angleY) ?? 0, double.tryParse(angleZ) ?? 0);
+        car.updateTransform();
       });
-      
     });
-     refY.onValue.listen((DatabaseEvent event){
+    refY.onValue.listen((DatabaseEvent event) {
       setState(() {
-      angleY = event.snapshot.value.toString();
+        angleY = event.snapshot.value.toString();
+        car.rotation.setValues(double.tryParse(angleX) ?? 0,
+            double.tryParse(angleY) ?? 0, double.tryParse(angleZ) ?? 0);
+        car.updateTransform();
       });
-      
     });
-     refZ.onValue.listen((DatabaseEvent event){
+    refZ.onValue.listen((DatabaseEvent event) {
       setState(() {
-      angleZ = event.snapshot.value.toString();
+        angleZ = event.snapshot.value.toString();
+        car.rotation.setValues(double.tryParse(angleX) ?? 0,
+            double.tryParse(angleY) ?? 0, double.tryParse(angleZ) ?? 0);
+        car.updateTransform();
       });
-      
     });
   }
 
@@ -53,18 +60,15 @@ DatabaseReference refZ = FirebaseDatabase.instance.ref("gyro/anglez");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text('FragileAT'),
-          backgroundColor: Colors.green
-      ),
-      body: Center(
-          child: Column(
-            children: <Widget>[
-              Text('angleX $angleX', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-              Text('angleY $angleY', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-              Text('angleZ $angleZ', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-            ],
-          )),
-    );
+        appBar: AppBar(
+            title: const Text('FragileAT'), backgroundColor: Colors.green),
+        body: Center(
+            child: Cube(
+          interactive: false,
+          onSceneCreated: (Scene s) {
+            s.world.add(car);
+            s.camera.zoom = 5;
+          },
+        )));
   }
 }
